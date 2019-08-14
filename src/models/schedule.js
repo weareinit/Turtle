8ee8481c4f829c58ed1865f8f97acd534924d0a3
Tommy Carrascal <Carrascalx@gmail.com>
2019-08-14 13:50:39 -0400

    starting work

--- src/models/schedule.js
+++ src/models/schedule.js
@@ -4,8 +4,8 @@ const scheduleSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   host: { type: String, required: true },
-  logo: { type: String },
-  category: { type: String },
+  timeCreated: { type: String},
+  date: { type: String, required: true },
   startTime: { type: String, required: true },
   endTime: { type: String, required: true }
 });
